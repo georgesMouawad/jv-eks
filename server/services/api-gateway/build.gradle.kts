fb0f4681c593d81ce10f8d11ff1a22de13f4c859
georgesMouawad <g.m.6@live.com>
2026-04-26 18:36:11 +0300

    build: initial

---
+++ server/services/api-gateway/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.devops.gateway"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.1.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ server/services/api-gateway/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "services-api-gateway"
